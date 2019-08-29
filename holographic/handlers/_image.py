42fb1220efce8d6228dc502174ab98a3919f312d
adamasstokhorst <adamas.aqsa@gmail.com>
2019-08-29 16:28:52 +0800

    Implement reapplication of image's average color value

--- holographic/handlers/_image.py
+++ holographic/handlers/_image.py
@@ -23,6 +23,9 @@ class ImageHandler(object):
             self._height = self._f.width
             self._width = self._f.height
             self._num_bands = len(color_mode)  # hack
+
+            self._get_padding()
+            self._get_average()
         elif self._mode == 'w':
             self._f = Image.new(mode=color_mode, size=(1, 1))
             self._fn = fn
@@ -31,22 +34,42 @@ class ImageHandler(object):
             self._num_bands = len(color_mode)  # hack
 
     def __call__(self, *args, **kwargs):
-        if self._lr_blocks is None:
-            right_pad = self._block_width - self._width % self._block_width
-            right_pad -= self._block_width if right_pad == self._block_width else 0
-            bottom_pad = self._block_width - self._height % self._block_width
-            bottom_pad -= self._block_width if bottom_pad == self._block_width else 0
-            lr_blocks = (self._width + right_pad) / self._block_width
-            ud_blocks = (self._height + bottom_pad) / self._block_width
-            self._lr_blocks = lr_blocks
-            self._ud_blocks = ud_blocks
-            self._bottom_pad = bottom_pad
-            self._right_pad = right_pad
-
         if self._mode == 'r':
             return self._read_func()
         else:
             self._write_func(*args)
+
+    def _get_padding(self):
+        right_pad = self._block_width - self._width % self._block_width
+        right_pad -= self._block_width if right_pad == self._block_width else 0
+        bottom_pad = self._block_width - self._height % self._block_width
+        bottom_pad -= self._block_width if bottom_pad == self._block_width else 0
+        lr_blocks = (self._width + right_pad) / self._block_width
+        ud_blocks = (self._height + bottom_pad) / self._block_width
+        self._lr_blocks = lr_blocks
+        self._ud_blocks = ud_blocks
+        self._bottom_pad = bottom_pad
+        self._right_pad = right_pad
+
+    def _get_average(self):
+        lr_blocks = self._lr_blocks
+        ud_blocks = self._ud_blocks
+        bottom_pad = self._bottom_pad
+        right_pad = self._right_pad
+
+        pixels = [numpy.array(list(self._f.getdata(band=x))) for x in xrange(self._num_bands)]
+        for i, p in enumerate(pixels):
+            p.shape = self._width, self._height
+            pixels[i] = numpy.pad(p, ((0, right_pad), (0, bottom_pad)), 'edge') / 128.0 - 1
+
+        self._average = [numpy.zeros((1, self._block_size)) for i in xrange(self._num_bands)]
+        for i, j in [(x, y) for x in xrange(ud_blocks) for y in xrange(lr_blocks)]:
+            for k, p in enumerate(pixels):
+                self._average[k] += p[j*self._block_width:(j+1)*self._block_width, i*self._block_width:(i+1)*self._block_width].flatten()
+
+        for a in self._average:
+            a /= lr_blocks * ud_blocks
+            a.shape = (a.size,)
 
     def _read_func(self):
         lr_blocks = self._lr_blocks
@@ -59,23 +82,20 @@ class ImageHandler(object):
             p.shape = self._width, self._height
             pixels[i] = numpy.pad(p, ((0, right_pad), (0, bottom_pad)), 'edge') / 128.0 - 1
 
-        self._average = numpy.zeros((1, self._block_size))
         for i, j in [(x, y) for x in xrange(ud_blocks) for y in xrange(lr_blocks)]:
-            for p in pixels:
-                self._average += p[j*self._block_width:(j+1)*self._block_width, i*self._block_width:(i+1)*self._block_width].flatten()
-        self._average /= lr_blocks * ud_blocks
-        for i, j in [(x, y) for x in xrange(ud_blocks) for y in xrange(lr_blocks)]:
-            yield [list(p[j*self._block_width:(j+1)*self._block_width, i*self._block_width:(i+1)*self._block_width].flatten()) for p in pixels]
+            d = [p[j*self._block_width:(j+1)*self._block_width, i*self._block_width:(i+1)*self._block_width].flatten() for p in pixels]
+            yield [list(a - b) for a, b in zip(d, self._average)]
+
         self._f.close()
 
     def _write_func(self, *args):
-        lr_blocks = self._lr_blocks
+        # lr_blocks = self._lr_blocks
         ud_blocks = self._ud_blocks
 
         # provide multiple arguments to write all of them in one go
         for arg in args:
             # for writing, set params first before calling
-            pixels = reduce(lambda a, b: a + b, zip(*arg))
+            pixels = reduce(lambda a, b: a + b, zip(*[x+y for x, y in zip(arg, self._average)]))
             pixels = [band_filter(128*(x + 1), 0, 255) for x in pixels]
             pixels = numpy.array(pixels, dtype='uint8')
 
@@ -94,11 +114,13 @@ class ImageHandler(object):
 
     def params(self, *args):
         if self._mode == 'r':
-            return self._height, self._width
+            return self._height, self._width, self._average
         elif self._mode == 'w':
             self._width = args[0]
             self._height = args[1]
-            self._f = self._f.resize(args)
+            self._average = args[2]
+            self._f = self._f.resize(args[:2])
+            self._get_padding()
 
     def close(self):
         if self._mode == 'r':
