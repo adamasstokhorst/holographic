78c08fbfda1f192150c50a78f4847a16a0121321
fredezerman <frederic.ezerman@gmail.com>
2019-09-19 12:46:19 +0800

    chnages in plot lambda
    adjusting the line thickness in the plot

--- plot_lambdas.py
+++ plot_lambdas.py
@@ -2,7 +2,7 @@ import os
 import holographic as hl
 from matplotlib import pyplot
 
-big_m = 16
+big_m = 256
 # small_m = 8
 # big_n = 8
 # sigma = 0.01
@@ -25,12 +25,12 @@ for i, fn in enumerate(fns):
     
 print 'Calculating aggregate statistics...'
 lamda, _ = hl.statistic.calculate_statistic(big_m, hl.ImageHandler, *fns)
-axes.semilogy(range(1, big_m+1), lamda, label='aggregate', color='black', linestyle='-')
+axes.semilogy(range(1, big_m+1), lamda, label='aggregate', color='black', linestyle='-', linewidth=5)
 
 axes.grid(True, linestyle='dotted')
-axes.set_ylabel('(Log) Lambda values', fontsize=24)
+axes.set_ylabel('Lambda values', fontsize=28)
 axes.set_xlabel(r'$i$', fontsize=24)
-axes.set_title('Lambda plots for various images', fontsize=32)
+axes.set_title('Lambda Plots for Images when M=256', fontsize=32)
 axes.legend(fontsize=8, ncol=5)
-pyplot.savefig('lambda_plots_M16.png')
+pyplot.savefig('lambda_plots_M256.png')
 pyplot.show()
--- single_image_buildup.py
+++ single_image_buildup.py
@@ -7,8 +7,8 @@ small_m = 8
 big_n = 8
 sigma = 0.01
 
-fn = 'koi.jpeg'
-use_im_stats = True
+fn = 'baby.jpg'
+use_im_stats = False
 
 if use_im_stats:
     # using image-specific statistics
@@ -24,7 +24,7 @@ sp = random.sample(range(big_n), big_n)
 for i in xrange(1, big_n + 1):
     print i,
     image_in = hl.ImageHandler('img/' + fn, big_m, mode='r', color_mode='RGB')
-    image_out = hl.ImageHandler('true_bout_{}_{}'.format(i, fn), big_m, mode='w', color_mode='RGB')
+    image_out = hl.ImageHandler('avg_bout_{}_{}'.format(i, fn), big_m, mode='w', color_mode='RGB')
     image_out.params(*image_in.params())
 
     for _, packet in hl.helpers.simulate(image_in(), psi, lamda, partitions, big_m, small_m, big_n, sigma, lost_space=sp[i:]):
