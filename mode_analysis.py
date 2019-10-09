b2d91f17d6fcfe5322dc9699e6366272fb951450
adamasstokhorst <adamas.aqsa@gmail.com>
2019-10-09 21:16:39 +0800

    Initial commit

---
+++ mode_analysis.py
@@ -0,0 +1,38 @@
+import pickle
+import itertools
+import collections
+import holographic as hl
+
+big_m = 64
+small_m = 8
+big_n = 8
+sigma = 0.01
+
+use_aggregate = True
+image_fn = ''
+mode = 1
+
+if use_aggregate:
+    with open('aggregate_statistics', 'r') as f:
+        d = pickle.load(f)
+        lamda, psi = d['lamda'], d['psi']
+    partitions = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma, lamda, mode=mode)
+else:
+    lamda, psi = hl.statistic.calculate_statistic(big_m, hl.ImageHandler, 'img/' + image_fn)
+    partitions = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma, lamda, mode=mode)
+
+baseline = sum(lamda)
+memo = {}
+result = []
+
+for ell in range(1, big_n+1):
+    best_sum = 0
+    for subset in itertools.combinations(range(big_n), ell):
+        subspace_counter = collections.Counter()
+        for i in subset:
+            subspace_counter.update(partitions[i])
+        sum_p = sum([hl.statistic.zeta(x, y, lamda, sigma, memo) for (x, y) in subspace_counter.most_common()])
+        best_sum = sum_p if sum_p > best_sum else best_sum
+    result.append(baseline - best_sum)
+
+print result
