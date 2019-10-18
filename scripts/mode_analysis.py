f058ac6ffb831ac01ad232dcb838d5729cf42c68
adamasstokhorst <adamas.aqsa@gmail.com>
2019-10-18 16:04:41 +0800

    Simplifying code to store all plots in one file

--- scripts/mode_analysis.py
+++ scripts/mode_analysis.py
@@ -10,34 +10,44 @@ sigma = 0.01
 
 use_aggregate = False
 image_fn = 'dragon.png'
-mode = 3
 
 if use_aggregate:
     with open('aggregate_statistics', 'r') as f:
         d = pickle.load(f)
-        lamda, psi = d['lamda'], d['psi']
-    partitions = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma, lamda, mode=mode)
+        lamda, _ = d['lamda'], d['psi']
 else:
-    lamda, psi = hl.statistic.calculate_statistic(big_m, hl.ImageHandler, 'img/' + image_fn)
+    lamda, _ = hl.statistic.calculate_statistic(big_m, hl.ImageHandler, 'img/' + image_fn)
+
+total_result = []
+
+# calculate best possible MSE reduction using each partition strategy
+for mode in range(1, 3+1):
     partitions = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma, lamda, mode=mode)
+    baseline = sum(lamda)
+    memo = {}
+    result = []
 
-baseline = sum(lamda)
-memo = {}
-result = []
+    for ell in range(1, big_n+1):
+        best_sum = 0
+        for subset in itertools.combinations(range(big_n), ell):
+            subspace_counter = collections.Counter()
+            for i in subset:
+                subspace_counter.update(partitions[i])
+            sum_p = sum([hl.statistic.zeta(x, y, lamda, sigma, memo) for (x, y) in subspace_counter.most_common()])
+            best_sum = sum_p if sum_p > best_sum else best_sum
+        result.append(baseline - best_sum)
 
-for ell in range(1, big_n+1):
-    best_sum = 0
-    for subset in itertools.combinations(range(big_n), ell):
-        subspace_counter = collections.Counter()
-        for i in subset:
-            subspace_counter.update(partitions[i])
-        sum_p = sum([hl.statistic.zeta(x, y, lamda, sigma, memo) for (x, y) in subspace_counter.most_common()])
-        best_sum = sum_p if sum_p > best_sum else best_sum
-    result.append(baseline - best_sum)
+    total_result.append(result)
 
 fnc = image_fn.split('.')[0]
-with open('modeanalysis_{}_mode{}'.format('aggregate' if use_aggregate else fnc, mode), 'w') as f:
-    pickle.dump({'x': range(1, big_n+1),
-                 'y': result,
+with open('modeanalysis_{}'.format('aggregate' if use_aggregate else fnc), 'w') as f:
+    pickle.dump({'param':
+                    {'big_m': big_m,
+                     'small_m': small_m,
+                     'big_n': big_n,
+                     'sigma': sigma},
                  'label': 'aggregate' if use_aggregate else fnc,
-                 'mode': mode}, f)
+                 'data':
+                    [{'x': range(1, big_n+1),
+                      'y': result,
+                      'mode': mode+1} for mode, result in enumerate(total_result)]}, f)
--- scripts/mode_plot.py
+++ scripts/mode_plot.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 from matplotlib import pyplot
+import holographic as hl
 
 fns = []
 for _, _, fns in os.walk('.\\'):
@@ -12,18 +13,40 @@ fnset = list(set([fn.split('_')[1] for fn in fns if 'aggregate' not in fn]))
 fig, axes = pyplot.subplots(1)
 fig.set_size_inches(12, 9)
 
+prev_param = None
+
 for fn in fns:
     with open(fn, 'r') as f:
-        data = pickle.load(f)
+        raw_data = pickle.load(f)
 
-    color = 'black' if data['label'] == 'aggregate' else pyplot.cm.get_cmap('brg', len(fnset))(fnset.index(data['label']))
-    label = '{} (mode {})'.format(data['label'], data['mode'])
-    mode = data['mode']
-    linestyle = '-.' if mode == 1 else '--' if mode == 2 else ':'
-    if data['label'] == 'aggregate':
-        axes.plot(data['x'], data['y'], label=label, color=color, linestyle=linestyle, linewidth=2)
-    else:
-        axes.plot(data['x'], data['y'], label=label, color=color, linestyle=linestyle)
+    data = raw_data['data']
+    name = raw_data['label']
+    param = raw_data['param']
+    
+    if param != prev_param and prev_param is not None:
+        print 'WARNING: parameter is not shared between all data'
+    
+    color = 'black' if name == 'aggregate' else pyplot.cm.get_cmap('brg', len(fnset))(fnset.index(name))
+    
+    for d in data:
+        mode = d['mode']
+        label = '{} (mode {})'.format(name, mode)
+        linestyle = '-.' if mode == 1 else '--' if mode == 2 else ':' if mode == 3 else '-'
+        
+        axes.plot(d['x'], d['y'], label=label, color=color, linestyle=linestyle)
+
+# add theoretical best possible MSE reduction
+big_m, big_n, small_m, sigma = param['big_m'], param['big_n'], param['small_m'], param['sigma']
+result = []
+with open('aggregate_statistics', 'r') as f:
+    d = pickle.load(f)
+    lamda, _ = d['lamda'], d['psi']
+for ell in range(1, big_n+1):
+    partitions = hl.statistic.calculate_partition(big_m, ell, small_m, sigma, lamda, mode=2)
+    big_l = max([max(p) for p in partitions])
+    print big_l, partitions
+    result.append(big_m * sigma / (1.0 * ell * small_m / big_m + 1.0 * sigma * sum([1.0 / lamda[i] for i in range(big_l)]) / big_l))
+axes.plot(range(1, big_n+1), result, label='theoretical best', color='black', linestyle='-', linewidth=1)
 
 axes.grid(True, linestyle='dotted')
 axes.set_ylabel('Mean squared error', fontsize=24)
