e0adf050b86cefbab352b887761a6e96fe78f27a
adamasstokhorst <adamas.aqsa@gmail.com>
2019-11-29 12:51:23 +0800

    Make sure script deals with new file format correctly

--- scripts/theoretical_analysis.py
+++ scripts/theoretical_analysis.py
@@ -1,8 +1,7 @@
-import pickle
 import itertools
 import collections
-import os
 import holographic as hl
+import ScriptHelper as Sh
 
 big_m = 64
 small_m = 8
@@ -12,19 +11,24 @@ sigma = 0.01
 use_aggregate = False
 image_fn = 'img/dragon.png'
 
+params = {'big_m': big_m,
+          'small_m': small_m,
+          'big_n': big_n,
+          'sigma': sigma}
+
 if use_aggregate:
-    with open('aggregate_statistics', 'r') as f:
-        d = pickle.load(f)
-        lamda, _ = d['lamda'], d['psi']
+    d = Sh.load_data('aggregate_statistics', params)
+    lamda, partitions = d['lamda'], d['partitions']
 else:
     lamda, _ = hl.statistic.calculate_statistic(big_m, hl.ImageHandler, image_fn)
+    partitions = {mode: hl.statistic.calculate_partition(big_m, big_n, small_m, sigma, lamda, mode=mode) for mode in range(1, 3+1)}
 
 total_result = []
 
 # calculate best possible MSE reduction using each partition strategy
 for mode in range(1, 4+1):
     if mode <= 3:
-        partitions = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma, lamda, mode=mode)
+        partition = partitions[mode]
         baseline = sum(lamda)
         memo = {}
         result = []
@@ -34,7 +38,7 @@ for mode in range(1, 4+1):
             for subset in itertools.combinations(range(big_n), ell):
                 subspace_counter = collections.Counter()
                 for i in subset:
-                    subspace_counter.update(partitions[i])
+                    subspace_counter.update(partition[i])
                 sum_p = sum([hl.statistic.zeta(x, y, lamda, sigma, memo) for (x, y) in subspace_counter.most_common()])
                 best_sum = sum_p if sum_p > best_sum else best_sum
             result.append(baseline - best_sum)
@@ -53,20 +57,10 @@ for mode in range(1, 4+1):
             result.append(sum([lm * sigma / (sigma + lm * z) for lm, z in zip(lamda[:big_l], zetas[:big_l])]) + sum(lamda[big_l:]))
         total_result.append(result)
 
-fnc = os.path.basename(image_fn).split('.')[0]
-try:
-    with open('modeanalysis_{}'.format('aggregate' if use_aggregate else fnc), 'r') as f:
-        data = pickle.load(f)
-except IOError:
-    data = {}
+data_fn = 'aggregate' if use_aggregate else Sh.get_fname(image_fn)
+data = {'label': 'aggregate' if use_aggregate else data_fn,
+        'data': [{'x': range(1, big_n+1),
+                  'y': result,
+                  'mode': mode+1} for mode, result in enumerate(total_result)]}
 
-with open('modeanalysis_{}'.format('aggregate' if use_aggregate else fnc), 'w') as f:
-    params = frozenset({'big_m': big_m,
-                        'small_m': small_m,
-                        'big_n': big_n,
-                        'sigma': sigma}.items())
-    data[params] = {'label': 'aggregate' if use_aggregate else fnc,
-                    'data': [{'x': range(1, big_n+1),
-                              'y': result,
-                              'mode': mode+1} for mode, result in enumerate(total_result)]}
-    pickle.dump(data, f)
+Sh.save_data('modeanalysis_{}'.format(data_fn), params, data)
