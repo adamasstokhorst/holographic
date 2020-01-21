6e26dc05283c3309139b4268d908ed5670079569
adamasstokhorst <adamas.aqsa@gmail.com>
2020-01-21 16:11:28 +0800

    Introduce multiprocessing to run tasks concurrently

--- scripts/simulated_analysis.py
+++ scripts/simulated_analysis.py
@@ -4,6 +4,23 @@ import random
 import numpy
 import holographic as hl
 import ScriptHelper as Sh
+import multiprocessing
+
+
+def worker_task(fn, bm, key, l, data_dict, params, ks, handler):
+    result = {'key': key,
+              'ell': l,
+              'out': []}
+    
+    for subset in ks:
+        if 'overall' in params:
+            data_dict['lost_space'] = subset
+        elif 'buildup' in params:
+            data_dict['lost_space'] = subset[l:]
+        for i_data, f_data in hl.helpers.simulate(handler(fn, bm, 'r')(), **data_dict):
+            result['out'].append(numpy.sum([(x - y)**2 for x, y in zip(i_data[0], f_data[0])]))
+    
+    return result
 
 
 def simulation_calc(p, pd, **kwargs):
@@ -20,6 +37,9 @@ def simulation_calc(p, pd, **kwargs):
     
     if 'buildup' in p:
         k_set = [random.sample(range(big_n), big_n) for _ in xrange(buildup_upper_limit)]
+
+    pool = multiprocessing.Pool()
+    futures = []
     
     for ell in range(1, big_n + 1):
         print ell,
@@ -35,24 +55,21 @@ def simulation_calc(p, pd, **kwargs):
                     sample = random.sample(range(big_n), big_n - ell)
                     if sample not in k_set:
                         k_set.append(sample)
-
-        total_mse = {k: list() for k in pd}
-
-        for subset in k_set:
-            if 'overall' in p:
-                pd['grid']['lost_space'] = pd['aggregate']['lost_space'] = subset
-                pd['img']['lost_space'] = pd['line']['lost_space'] = subset
-            elif 'buildup' in p:
-                pd['grid']['lost_space'] = pd['aggregate']['lost_space'] = subset[ell:]
-                pd['img']['lost_space'] = pd['line']['lost_space'] = subset[ell:]
-                
-            for k in total_mse:
-                for i_data, f_data in hl.helpers.simulate(handler(fname, big_m, 'r')(), **pd[k]):
-                    in_vec, out_vec = numpy.array(i_data), numpy.array(f_data)
-                    total_mse[k].append(numpy.mean((in_vec - out_vec)**2))
+        
+        for k in pd:
+            futures.append(pool.apply_async(worker_task, (fname, big_m, k, ell, pd[k], p, k_set, handler)))
 
         x_points.append(ell)
-        for k, v in total_mse.items():
+    
+    total_result = collections.defaultdict(list)
+    for future in futures:
+        val = future.get()
+        total_result[val['key']].append((val['ell'], val['out']))
+    
+    for k, ord_list in total_result.items():
+        ord_list.sort()
+        vals = zip(*ord_list)[1]
+        for v in vals:
             y_points[k]['avg'].append(numpy.mean(v))
             y_points[k]['var'].append(numpy.var(v))
 
@@ -63,49 +80,50 @@ def simulation_calc(p, pd, **kwargs):
     print
 
 
-# list of filepaths
-fnames = ['img/bratan.jpg', 'img/dragon.png', 'img/fly.jpeg', 'img/bees.jpg', 'img/dish.jpg', 'img/owl.jpg']
-# parameters
-big_m = 64
-small_m = 8
-big_n = 8
-sigma = 0.01
-mode = 1
-# number of combinations to sample
-buildup_upper_limit = 25
-overall_sampling_limit = float('inf')
+if __name__ == '__main__':
+    # list of filepaths
+    fnames = ['img/fly.jpeg']
+    # parameters
+    big_m = 64
+    small_m = 8
+    big_n = 8
+    sigma = 0.01
+    mode = 1
+    # number of combinations to sample
+    buildup_upper_limit = 25
+    overall_sampling_limit = float('inf')
 
-handler = hl.ImageHandler
+    handler = hl.ImageHandler
 
-print 'Calculating simulated recovery results...'
-print '  Parameters: M={}, m={}, N={}, sigma^2={} on mode {}'.format(big_m, small_m, big_n, sigma, mode)
+    print 'Calculating simulated recovery results...'
+    print '  Parameters: M={}, m={}, N={}, sigma^2={} on mode {}'.format(big_m, small_m, big_n, sigma, mode)
 
-params = {'big_m': big_m, 'small_m': small_m, 'big_n': big_n, 'sigma': sigma}
+    params = {'big_m': big_m, 'small_m': small_m, 'big_n': big_n, 'sigma': sigma}
 
-# Prepare all the parameters
-param_dict = {'aggregate': Sh.load_data('aggregate_statistics', params)}
-param_dict['aggregate']['partitions'] = param_dict['aggregate']['partitions'][mode]
-param_dict['aggregate'].update(params)
+    # Prepare all the parameters
+    param_dict = {'aggregate': Sh.load_data('aggregate_statistics', params)}
+    param_dict['aggregate']['partitions'] = param_dict['aggregate']['partitions'][mode]
+    param_dict['aggregate'].update(params)
 
-param_dict['grid'] = dict(param_dict['aggregate'])
-param_dict['grid']['lamda'], param_dict['grid']['psi'] = hl.statistic.get_lp_lambda(1, big_m)
-param_dict['grid']['partitions'] = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma,
-                                                                    param_dict['grid']['lamda'], mode=mode)
+    param_dict['grid'] = dict(param_dict['aggregate'])
+    param_dict['grid']['lamda'], param_dict['grid']['psi'] = hl.statistic.get_lp_lambda(1, big_m)
+    param_dict['grid']['partitions'] = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma,
+                                                                        param_dict['grid']['lamda'], mode=mode)
 
-param_dict['line'] = dict(param_dict['aggregate'])
-param_dict['line']['lamda'], param_dict['line']['psi'] = hl.statistic.get_lp_lambda(2, big_m)
-param_dict['line']['partitions'] = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma,
-                                                                    param_dict['line']['lamda'], mode=mode)
+    param_dict['line'] = dict(param_dict['aggregate'])
+    param_dict['line']['lamda'], param_dict['line']['psi'] = hl.statistic.get_lp_lambda(2, big_m)
+    param_dict['line']['partitions'] = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma,
+                                                                        param_dict['line']['lamda'], mode=mode)
 
-params['mode'] = mode
+    params['mode'] = mode
 
-for fname in fnames:
-    # make a copy for image-specific statistics
-    param_dict['img'] = dict(param_dict['aggregate'])
-    param_dict['img']['lamda'], param_dict['img']['psi'] = hl.statistic.calculate_statistic(big_m, hl.ImageHandler, fname)
-    param_dict['img']['partitions'] = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma,
-                                                                       param_dict['img']['lamda'], mode=mode)
-    im_name = Sh.get_fname(fname)
-    
-    simulation_calc(params, param_dict, buildup=True)
-    simulation_calc(params, param_dict, overall=True)
+    for fname in fnames:
+        # make a copy for image-specific statistics
+        param_dict['img'] = dict(param_dict['aggregate'])
+        param_dict['img']['lamda'], param_dict['img']['psi'] = hl.statistic.calculate_statistic(big_m, hl.ImageHandler, fname)
+        param_dict['img']['partitions'] = hl.statistic.calculate_partition(big_m, big_n, small_m, sigma,
+                                                                           param_dict['img']['lamda'], mode=mode)
+        im_name = Sh.get_fname(fname)
+        
+        simulation_calc(params, param_dict, buildup=True)
+        simulation_calc(params, param_dict, overall=True)
