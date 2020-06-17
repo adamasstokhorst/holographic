980ae28de7ec37ab7a1194a0f8c4b33efc73e4ee
adamasstokhorst <adamas.aqsa@gmail.com>
2020-06-17 10:17:07 +0800

    Apply DCT to data before emitting

--- holographic/handlers/_wave.py
+++ holographic/handlers/_wave.py
@@ -1,5 +1,6 @@
 import wave
 import struct
+from scipy.fftpack import dct, idct
 
 from ..helpers import band_filter
 
@@ -40,24 +41,27 @@ class WaveHandler(object):
                     padding = [0] * (-num_frames)
                 frames = map(''.join, (zip(*[iter(data_bytes)] * m)))
                 audio_data = [frames[i::p] for i in xrange(p)]
-                yield [[struct.unpack(struct_str, y)[0] / l - e for y in x] + padding for x in audio_data]
+                data = [[struct.unpack(struct_str, y)[0] / l - e for y in x] + padding for x in audio_data]
+                yield dct(data, norm='ortho')
             s.close()
         return _data_generator(self._f, self._block_size, sampwidth, num_channel, high, mean, ss)
 
     def _write_func(self, sampwidth, num_channel, ss, high, mean, *args):
         # for writing, set params first before calling
-        if len(args) != num_channel:
-            raise ValueError('input mismatches number of channels. (got {})'.format(len(args)))
-        data_bytes = []
-        for i in xrange(num_channel):
-            data_bytes.append([struct.pack(ss, high * (band_filter(x, -1, 1) + mean)) for x in args[i]])
-        self._f.writeframes(''.join([val for tup in zip(*data_bytes) for val in tup]))
+        for arg in args:
+            if len(arg) != num_channel:
+                raise ValueError('input mismatches number of channels. (got {})'.format(len(arg)))
+            data_bytes = []
+            for i in xrange(num_channel):
+                recov_data = idct(arg[i], norm='ortho')
+                data_bytes.append([struct.pack(ss, high * (band_filter(x, -1, 1) + mean)) for x in recov_data])
+            self._f.writeframes(''.join([val for tup in zip(*data_bytes) for val in tup]))
 
     def params(self, *args):
         if self._mode == 'r':
             return self._f.getparams()
         elif self._mode == 'w':
-            self._f.setparams(args[0])
+            self._f.setparams(args)
 
     def close(self):
         if self._mode == 'r':
