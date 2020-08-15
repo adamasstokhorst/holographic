af90802e0091c424b2443df00589e0e54960fb88
fredezerman <51964982+fredezerman@users.noreply.github.com>
2020-08-15 23:45:21 +0800

    Update plot_lambdas.py
    change legend and title

--- scripts/plot_lambdas.py
+++ scripts/plot_lambdas.py
@@ -31,7 +31,7 @@ else:
     fns = all_fns
 
 fig, axes = pyplot.subplots(1)
-fig.set_size_inches(10, 6)
+fig.set_size_inches(10, 6.5)
 fig.set_dpi(200)
 
 for i, fn in enumerate(fns):
@@ -47,9 +47,10 @@ axes.semilogy(range(1, big_m+1), lamda, label='aggregate', color='black', linest
 axes.grid(True, linestyle='dotted')
 axes.set_ylabel(r'(Log) $\lambda_j$ values', fontsize=28)
 axes.set_xlabel(r'$j$', fontsize=24)
-axes.set_title(r"""\Huge Lambda plots for various images
-                   \Large $\left ( M={} \right )$""".format(big_m))
-axes.legend(fontsize=8, ncol=5)
+#axes.set_title(r"""\Huge Lambda plots for various images
+#                   \Large $\left ( M={} \right )$""".format(big_m))
+axes.set_title(r'Lambda plots for various images when $M={}$'.format(big_m), fontsize=28)
+axes.legend(fontsize=9, ncol=5)
 pyplot.savefig('lambda_plots.svg')
 
 print 'Saved to lambda_plots.svg'
